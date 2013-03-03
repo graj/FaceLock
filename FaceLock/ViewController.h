64fd267deddd31215b461c07317965fa10fde6ba
Diogo Carneiro <contato@idiogo.com.br>
2013-03-03 15:59:43 -0300

    Initial Commit

---
+++ FaceLock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FaceLock
+//
+//  Created by Diogo Carneiro on 03/03/13.
+//  Copyright (c) 2013 Diogo Carneiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FaceLock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FaceLock
+//
+//  Created by Diogo Carneiro on 03/03/13.
+//  Copyright (c) 2013 Diogo Carneiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
